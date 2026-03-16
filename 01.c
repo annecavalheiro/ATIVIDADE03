e569614e821af10a222dafb4283fa618aeb76108
Anne Cavalheiro <annecavalheiro@gmail.com>
2026-03-16 19:00:09 -0400

    Update 01.c

--- 01.c
+++ 01.c
@@ -1,11 +1,9 @@
+
 // QUESTÃO 1 - Exibir a soma dos números positivos no intervalo de 1 a 100
 #include <stdio.h>
+
 int main() {
-    int i, soma = 0;
-    for(i = 1; i <= 100; i++) {
-        soma += i;
-    }
+    int soma = 100 * 101 / 2;
     printf("A soma dos numeros de 1 a 100 eh: %d\n", soma);
     return 0;
 }
-
